4dc2e3ad4806b25d528c81428d5cb9b3e1cfaafc
İsmail Umutluoğlu <160246340+ismailumutluoglu@users.noreply.github.com>
2024-12-14 19:35:10 +0300

    Update RockPapperScissor.py

--- RockPapperScissor.py
+++ RockPapperScissor.py
@@ -1,6 +1,5 @@
 import random
 
-#Print multiline instruction
 print("Winning rules of the game ROCK PAPER SCISSORS are :\n"
       "Rock vs Paper -----> Paper wins \n"
       "Rock vs Scissors -----> Rock wins \n"
@@ -10,14 +9,14 @@ while True:
 
     print("Enter your choice \n 1) Rock \n 2) Paper \n 3) Scissors \n")
 
-    # Take the input from user
+   
     choice = int(input("Enter your choice: "))
 
-    # Looping until user enters valid input
+    
     while choice > 3 or choice < 1:
         choice = int(input('Enter a valid choice please : '))
 
-    # Initialize value of choice_name variable corresponding to the choice value
+  
     if choice == 1:
         choice_name = 'Rock'
     elif choice == 2:
@@ -25,14 +24,14 @@ while True:
     else:
         choice_name = 'Scissors'
 
-    # Print user choice
+ 
     print('User choice is:', choice_name)
     print("Now it's Computer's Turn...")
 
-    # Computer chooses randomly any number among 1, 2, and 3
+  
     comp_choice = random.randint(1, 3)
 
-    # Initialize value of comp_choice_name variable corresponding to the choice value
+    
     if comp_choice == 1:
         comp_choice_name = 'Rock'
     elif comp_choice == 2:
@@ -43,7 +42,7 @@ while True:
     print("Computer choice is:", comp_choice_name)
     print(choice_name, 'vs', comp_choice_name)
 
-    # Determine the winner
+   
     if choice == comp_choice:
         result = "DRAW"
     elif (choice == 1 and comp_choice == 2) or (comp_choice == 1 and choice == 2):
